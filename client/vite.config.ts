9cf4a3e479c64ef0a22cbe9c42452c58a72c724f
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2023-01-05 02:21:29 +0800

    update

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,13 +5,6 @@ import eslintConfig from './.eslintrc.json';
 import type { Linter } from 'eslint';
 import { resolve } from 'path';
 
-if (
-    typeof process.env.VITE_SERVER_URL === 'string' &&
-    process.env.VITE_SERVER_URL.length > 0
-) {
-    import.meta.env.VITE_SERVER_URL = process.env.VITE_SERVER_URL;
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
